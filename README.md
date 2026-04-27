7481b1807050834a51158fcfbe7be96555ab9420
emir12311 <12345emir15254@gmail.com>
2026-04-27 21:51:26 +0300

    add ft_print_comb and detail readme

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 Hello people! This is just my way of making the library they make you do in the Piscine at 42. Since I am planning to head to 42 Kocaeli after high school, I wanted to start early on the logic and the rules they have. It's definitely different from my usual projects, but it's been great for learning how things work under the hood.
 
 ## Whats inside
-- ft_putchar
-- ft_print_alphabet
-- ft_print_reverse_alphabet
-- ft_print_numbers
-- ft_is_negative
+- ft_putchar (ex00)
+- ft_print_alphabet (ex01)
+- ft_print_reverse_alphabet (ex02)
+- ft_print_numbers (ex03)
+- ft_is_negative (ex04)
+- ft_print_comb (ex05)
 
 (More will come..)
--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -1,25 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emir12311 <emir12311.dev@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/26 20:54:48 by emir12311         #+#    #+#             */
-/*   Updated: 2026/04/27 20:47:35 by emir12311        ###   ########.fr       */
+/*   Created: 2026/04/27 20:50:36 by emir12311         #+#    #+#             */
+/*   Updated: 2026/04/27 21:50:53 by emir12311        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_reverse_alphabet(void)
+void	ft_display_comb(char x, char y, char z)
 {
-	char	letter;
-
-	letter = 'z';
-	while (letter >= 'a')
+	write(1, &x, 1);
+	write(1, &y, 1);
+	write(1, &z, 1);
+	if (!(x == '7' && y == '8' && z == '9'))
 	{
-		write(1, &letter, 1);
-		letter--;
+		write(1, ", ", 2);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	x;
+	char	y;
+	char	z;
+
+	x = '0';
+	while (x <= '7')
+	{
+		y = x + 1;
+		while (y <= '8')
+		{
+			z = y + 1;
+			while (z <= '9')
+			{
+				ft_display_comb(x, y, z);
+				z++;
+			}
+			y++;
+		}
+		x++;
 	}
 }
